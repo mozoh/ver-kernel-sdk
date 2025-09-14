71b10c67971fc670bf99343de4afc26b37f4462f
temior <temior@gmx.com>
2025-09-14 07:58:13 +0300

    qfix

--- Assets/Scripts/AddressablePaths.cs
+++ Assets/Scripts/AddressablePaths.cs
@@ -1,6 +1,6 @@
 public static class KernelAddressablePaths {
 #if UNITY_ANDROID
-    public static string LoadPath => "{UnityEngine.Application.persistentDataPath}/AssetBundles/Custom";
+    public static string LoadPath => "{UnityEngine.Application.persistentDataPath}";
 #else
     public static string LoadPath => "AssetBundles/StandaloneWindows64/Custom";
 #endif
